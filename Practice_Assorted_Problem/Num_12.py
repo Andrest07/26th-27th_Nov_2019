95bb23e2f9395c63cce825dda3b08b8f2cb7e4f2
Andrest07 <57931603+Andrest07@users.noreply.github.com>
2019-11-26 10:42:03 +0700

    Add files via upload

---
+++ Practice_Assorted_Problem/Num_12.py
@@ -0,0 +1,10 @@
+def encrypt(text, s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + s - 65) % 26 + 65)
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
+print("Cipher: " + encrypt(input("Please input the text:"), int(input("Please input the shift:"))))
